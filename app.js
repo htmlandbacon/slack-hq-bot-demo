4fe04f531db7b0f2d82c66cd2a25163d07e60bcd
Colin Oakley <colin@htmlandbacon.com>
2015-06-14 16:20:41 +0100

    added hello example

--- app.js
+++ app.js
@@ -15,6 +15,20 @@ app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(400).send(err.message);
 });
+
+app.post('/hello', function (req, res) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text : 'Hello, ' + userName + '!'
+  };
+ 
+  // avoid infinite loop
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+});
  
 app.listen(port, function () {
   console.log('Server listening on port ' + port);
