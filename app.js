3ad27ba30ff550027a0a3f7ad0cc95bb8c1c496f
Colin Oakley <colin@htmlandbacon.com>
2016-01-26 21:37:37 +0000

    added random service return

--- app.js
+++ app.js
@@ -1,54 +1,72 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var rp = require('request-promise');
+var _ = require('underscore');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
 // body parser middleware
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // test route
-app.get('/', function (req, res) { res.status(200).send('Hello world!') });
+app.get('/', function (req, res) {
+  res.status(200).send('Hello world!');
+});
 
 // error handler
 app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(400).send(err.message);
+  next();
 });
 
 app.post('/hello', function (req, res) {
   var userName = req.body.user_name;
   var botPayload = {
-    text : 'Hello, ' + userName + '!'
+    text: 'Hello, ' + userName + '!'
   };
-
-
 
   // avoid infinite loop
   if (userName !== 'slackbot') {
     return res.status(200).json(botPayload);
-  } else {
-    return res.status(200).end();
   }
+  return res.status(200).end();
 });
 
-
 app.use('/benism', function (req, res) {
-
   var benism = require('./ben.json');
 
-  var randomNumber = Math.floor(Math.random() * ((benism.length-1) - 0 + 1)) + 0;
+  var randomNumber = Math.floor(Math.random() * ((benism.length - 1) - 0 + 1)) + 0;
 
   var userName = req.body.user_name;
   var botPayload = {
-    text : benism[randomNumber]
+    text: benism[randomNumber]
   };
   // avoid infinite loop
   if (userName !== 'slackbot') {
     return res.status(200).json(botPayload);
-  } else {
-    return res.status(200).end();
   }
+  return res.status(200).end();
+});
+
+app.get('/services', function (req, res) {
+  rp('http://dwp-digital-services.herokuapp.com/api').then(function (data) {
+    var dataset = JSON.parse(data);
+    var datasetSorted = _.filter(dataset, {phase: 'beta'});
+    var random = _.sample(datasetSorted, 1);
+    var userName = req.body.user_name;
+    var botPayload = 'Service: ' + random[0].name + ' - ' + random[0].description;
+    if (random[0].phase_modifier !== undefined) {
+      botPayload = botPayload + ' (' + random[0].phase_modifier + ')';
+    }
+
+    // avoid infinite loop
+    if (userName !== 'slackbot') {
+      return res.status(200).json({text: botPayload});
+    }
+    return res.status(200).end();
+  });
 });
 
 app.listen(port, function () {
